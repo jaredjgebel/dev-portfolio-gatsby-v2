6deb8189f36cbe225a8ae4f07abb72d468d688c4
Jared <jaredjgebel@gmail.com>
2020-09-03 16:37:41 -0400

    Update packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,6 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-plugin-styled-components`
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
